0fe956f594bda05f62a7308fcfe7da426793e77d
Evan Guirino <39364959+etg624@users.noreply.github.com>
2019-12-16 17:13:28 -0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-export const STREAM_API_KEY = '26fs9qr3hv3g';
-// const STREAM_API_TOKEN =
-export const STREAM_APP_ID = '65617';
-export const STREAM_API_SECRET = 'bvpkxpx9j85qsgafzy7pwcgw3fmnmjq4nnyvn3pkbvryutsvq6zk534dbmymw7b8';
